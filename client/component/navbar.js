9b37baacd00727fc41c6aea2eee50c2cc6321d0a
Prashant-jakhad <97466823+Prashant01102004@users.noreply.github.com>
2023-05-13 11:16:03 +0530

    my-orders

--- client/component/navbar.js
+++ client/component/navbar.js
@@ -12,9 +12,9 @@ export default function Navbar() {
                 />
                 <h2 className="header--title"><a href="/">Apt-System</a></h2>
                 <h4 className="header--project1"><a href="/">Home</a></h4>
-                <h4 className="header--project2"><a href="/">MyOrder</a></h4>
+                <h4 className="header--project2"><a href="/my-order">MyOrder</a></h4>
                 <h4 className="header--project2"><a href="/">MyPropertyActivity</a></h4>
-                <h4 className="header--project7"><a href="/">Contact Us</a></h4>
+                <h4 className="header--project7"><a href="/contact">Contact Us</a></h4>
                 <h4 className="header--project7"><a href="/profile"><img className="rounded-circle" src={session.user.image}  width="150" alt='not found' ></img></a></h4>
             </header>
         );
@@ -28,9 +28,9 @@ export default function Navbar() {
                 <h2 className="header--title"><a href="/">Apt-System</a></h2>
                 <h4 className="header--project1"><a href="/">Home</a></h4>
 
-                <h4 className="header--project2"><a href="/">MyOrder</a></h4>
+                <h4 className="header--project2"><a href="/my-order">MyOrder</a></h4>
                 <h4 className="header--project2"><a href="/">MyPropertyActivity</a></h4>
-                <h4 className="header--project7"><a href="/">Contact Us</a></h4>
+                <h4 className="header--project7"><a href="/contact">Contact Us</a></h4>
                 <h4 className="header--project7"><a href="/profile"><img  className="rounded-circle" src="https://www.pngkey.com/png/detail/202-2024792_profile-icon-png.png" width="150" alt="not found"></img></a></h4>
             </header>
         );
